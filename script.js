55f3922792d6d6fd1a964ed9beff1b05cda4f062
Trent Wintermeier <112954339+trentwintermeier@users.noreply.github.com>
2025-07-11 09:10:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -31,9 +31,18 @@ document.body.addEventListener('click', () => {
   });
 });
 startOverlay.addEventListener('click', () => {
-  music.play().catch(() => {
-    console.log("Music autoplay blocked.");
+  // Try to play background music
+  music.play().then(() => {
+    console.log("Music playing");
+  }).catch((err) => {
+    console.error("Music play blocked:", err);
   });
+
+  // (continue with rest of your app logic here)
+  startOverlay.classList.add('fade');
+  setTimeout(() => startOverlay.remove(), 600);
+
+  // Example: activate camera or livestream here
 });
 
 
